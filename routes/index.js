3b08786ae10180a2e6836512b7dcd5d85bd74bfe
Dave Edelhart <dave@wonderlandlabs.com>
2016-08-03 12:06:43 -0700

    adding info page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
+"use strict";
+
 var express = require('express');
 var router = express.Router();
 var config = require('./../config.json');
+var info = require('./../info.json');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: config.title });
+router.get('/', function (req, res, next) {
+    res.render('index', config);
+});
+
+router.get('/info', (req, res) => {
+    res.render('info', info);
 });
 
 module.exports = router;
