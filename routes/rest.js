2c9d2bc90b62f9feb2d9c3a6cd78114119bb77de
Dave Edelhart <dave@wonderlandlabs.com>
2016-08-04 10:17:27 -0700

    requiring channel in message.

--- routes/rest.js
+++ routes/rest.js
@@ -9,11 +9,15 @@ const manager = new bottle.container.Manager(reg);
 manager.createGame('sampleChannel', 'sampleUser1', 'sampleUser2');
 
 /* GET users listing. */
-router.post('/game/create', function(req, res, next) {
+router.post('/game/create', function (req, res, next) {
     const props = _.pick(req.body, 'channel,user1,user2'.split(','));
     console.log('creating game using ', req.body);
-    const game = manager.createGame(props.channel, props.user1, props.user2);
-    res.send(game.toJSON());
+    if (!req.body.channel) {
+        return res.status(400).send({error: 'no channel recieved'});
+    } else {
+        const game = manager.createGame(props.channel, props.user1, props.user2);
+        res.send(game.toJSON());
+    }
 });
 
 router.get('/', (req, res) => {
